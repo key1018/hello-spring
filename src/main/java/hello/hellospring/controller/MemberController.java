4341cac7154e8e12eeb361c2c2e1a76f4cc0cfb0
eunyoung <103404357+key1018@users.noreply.github.com>
2023-01-15 20:22:12 +0900

    feat : 화면 웹 기능 author : EunYoung
    - feat : createMemberForm.html
    - feat : home.html
    - feat : HomeController.java
    - feat : MemberController.java
    - feat : MemberForm.java
    - feat : memberList.html

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -31,4 +37,32 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new") // get방식으로 통해 Mapping됨
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    /* get과 post의 차이
+        - get : 조회할 때 주로 사용
+        - post : 데이터를 form에 넣어서 전달할 때 주로 사용
+     */
+
+    @PostMapping("/members/new") // url은 동일하지만 html에서 post방식으로 전달했기 때문에 해당 메소드가 값을 전달받음
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member : " + member.getName());
+        memberService.join(member);
+
+        return "redirect:/"; // 시작페이지로 다시 넘어감
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        // members안에 list로 모든 회원들을 조회해서 담아눔
+        return "members/memberList";
+    }
 }
